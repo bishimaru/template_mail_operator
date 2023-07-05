80f46f2e96de9996b592540a866176b927b5cab7
bishimaru <a414510nee@yahoo.co.jp>
2023-07-06 02:07:58 +0900

    7

--- kumi/repost_kumi.py
+++ kumi/repost_kumi.py
@@ -18,6 +18,7 @@ import sqlite3
 def repost_happymail_pcmax():
   adult_flag = True
   genre_flag = setting.genre_flag
+  genre_flag_pcmax = setting.genre_flag_pcmax
   name = "クミ"
   title = "リードします！楽しく会える人♪一般事務OLです。"
   text = """はじめまして！社会人二年目のくみです♪
@@ -44,7 +45,7 @@ def repost_happymail_pcmax():
     print('=== エラー内容 ===')
     print(traceback.format_exc())
   try:
-    pcmax.re_post(name, p_w, driver)
+    pcmax.re_post(name, p_w, driver, genre_flag_pcmax)
   except Exception as e:
     print('=== エラー内容 ===')
     print(traceback.format_exc())
