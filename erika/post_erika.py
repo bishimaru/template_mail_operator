c7930fdb88a7430a0d1d0cc63f661e193ed1bb7f
bishimaru <a414510nee@yahoo.co.jp>
2023-07-22 11:27:19 +0900

    auto commit

--- erika/post_erika.py
+++ erika/post_erika.py
@@ -47,11 +47,11 @@ def repost_happymail_pcmax():
   driver = webdriver.Chrome(service=service, options=options)
   h_w = func.get_windowhandle("happymail", name)
   p_w = func.get_windowhandle("pcmax", name)
-  # try:   
-  #   happymail.re_post(name, h_w, driver, title, text, adult_flag, genre_flag)
-  # except Exception as e:
-  #   print('=== エラー内容 ===')
-  #   print(traceback.format_exc())
+  try:   
+    happymail.re_post(name, h_w, driver, title, text, adult_flag, genre_flag)
+  except Exception as e:
+    print('=== エラー内容 ===')
+    print(traceback.format_exc())
   try:
     pcmax.re_post(name, p_w, driver, genre_flag_pcmax)
   except Exception as e:
