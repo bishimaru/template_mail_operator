164a1b9686cf95fcc9f41f919d0dd1b07c8d0b15
bishimaru <a414510nee@yahoo.co.jp>
2023-08-30 07:27:22 +0900

    auto commit

--- h_footprints01.py
+++ h_footprints01.py
@@ -27,7 +27,7 @@ def happymail_footprints(driver, wait):
   # # データ検索
   cur.execute('SELECT name, login_id, passward FROM happymail')
   happy_user_list = []
-  foot_order_list = ["えりか","くみ","あすか","りな", "めあり","りこ", "ハル","きりこ",  "ももか", "ゆうこ", "彩香", "ゆりあ", "まいこ", "haru"]
+  foot_order_list = ["えりか","あすか","りな", "めあり","りこ", "ハル","きりこ",  "ももか", "ゆうこ", "彩香", "haru"]
   for row in cur:
       # print(row[0])
       if row[0] in foot_order_list:
