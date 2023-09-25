7ee9c735eb0a78b11f1d14b77d54589e2e6b0be7
bishimaru <a414510nee@yahoo.co.jp>
2023-09-25 21:08:06 +0900

    auto commit

--- s_check_mail.py
+++ s_check_mail.py
@@ -33,9 +33,9 @@ order_list = [
    ["ゆうこ", "yuko414510@gmail.com"], 
    ["ハル", "haruru414510@gmail.com"],#足跡NG
 ]
-order_list = [
-  ["ゆうこ", "yuko414510@gmail.com"], 
-]
+# order_list = [
+#   ["ゆうこ", "yuko414510@gmail.com"], 
+# ]
 def get_driver(debug):
     options = Options()
     
@@ -61,29 +61,29 @@ def check_mail():
     debug = False
     new_mail_list = []
     #  # ハッピーメール
-    # try:
-    #     driver, wait = get_driver(debug)
-    #     happymail_new = happymail.check_new_mail(driver, wait, order_info[0])
-    #     if happymail_new:
-    #         new_mail_list.append(happymail_new)
-    #     print(1234)
-    #     print(happymail_new)
-    #     driver.quit()
-    # except Exception as e:
-    #     print(traceback.format_exc())
-    #     driver.quit()
-    # # pcmax
-    # try:
-    #     driver, wait = get_driver(debug)
-    #     pcmax_new = pcmax.check_new_mail(driver, wait, order_info[0])
-    #     print(5678)
-    #     print(pcmax_new)
-    #     if pcmax_new:
-    #         new_mail_list.append(pcmax_new)
-    #     driver.quit()
-    # except Exception as e:
-    #     print(traceback.format_exc())
-    #     driver.quit()
+    try:
+        driver, wait = get_driver(debug)
+        happymail_new = happymail.check_new_mail(driver, wait, order_info[0])
+        if happymail_new:
+            new_mail_list.append(happymail_new)
+        print(1234)
+        print(happymail_new)
+        driver.quit()
+    except Exception as e:
+        print(traceback.format_exc())
+        driver.quit()
+    # pcmax
+    try:
+        driver, wait = get_driver(debug)
+        pcmax_new = pcmax.check_new_mail(driver, wait, order_info[0])
+        print(5678)
+        print(pcmax_new)
+        if pcmax_new:
+            new_mail_list.append(pcmax_new)
+        driver.quit()
+    except Exception as e:
+        print(traceback.format_exc())
+        driver.quit()
     # gmail
     try:
         debug = True
