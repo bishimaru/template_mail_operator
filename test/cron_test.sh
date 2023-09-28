8f9f1b3518527071876f70fe72210ca877e2f324
bishimaru <a414510nee@yahoo.co.jp>
2023-09-28 22:43:13 +0900

    auto commit

--- test/cron_test.sh
+++ test/cron_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "777" > /Users/yamamotokenta/Desktop/myprojects/mail_tool/test/mohu.txt
+echo "777" >> /Users/yamamotokenta/Desktop/myprojects/mail_tool/test/mohu.txt >> /Users/yamamotokenta/Desktop/myprojects/mail_tool/test/cron.log 2>&1
