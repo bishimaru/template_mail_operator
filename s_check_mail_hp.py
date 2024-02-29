fca824bf4e8e5fb593fe46980d7571991cb557c6
bishimaru <a414510nee@yahoo.co.jp>
2024-02-29 16:30:44 +0900

    auto commit

--- s_check_mail_hp.py
+++ s_check_mail_hp.py
@@ -50,7 +50,7 @@ def get_driver(debug):
         options.add_argument("--remote-debugging-port=9222")
         options.add_argument('--headless')
     else:
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument("--incognito")
         options.add_argument("--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1")
     options.add_argument("--no-sandbox")
