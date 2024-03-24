5139d6371fcfd351f803492f8756eeaab8b7aeb5
bishimaru <a414510nee@yahoo.co.jp>
2024-03-24 22:57:59 +0900

    auto commit

---
+++ scheduler/h_schedule_test_aws.py
@@ -0,0 +1,33 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from widget import pcmax, happymail, func
+import sb_h_test_aws
+import s_check_mail_hp
+from datetime import datetime, timedelta
+
+
+
+def tick():
+    print("Tick! The time is : %s'" % datetime.now())
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()  # スケジューラを作る
+
+    # サイト回し
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=6, minute=30, args=[27], max_instances=1,misfire_grace_time=60*60)
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=8, minute=50, args=[18], max_instances=1,misfire_grace_time=60*60)
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=11, minute=0, args=[18], max_instances=1,misfire_grace_time=60*60)
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=15, minute=50, args=[18], max_instances=1,misfire_grace_time=60*60)
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=18, minute=0, args=[18], max_instances=1,misfire_grace_time=60*60)
+    scheduler.add_job(sb_h_test_aws.sb_h_all_do, 'cron', hour=19, minute=30, args=[18], max_instances=1,misfire_grace_time=60*60)
+
+    # scheduler.add_job(tick, 'interval', second=5)  
+    print("Press Ctrl+{0} to exit.".format('Break' if os.name == 'nt' else 'C'))
+    
+    try:
+        scheduler.start()  # スタート
+    except (KeyboardInterrupt, SystemExit):
+        pass
